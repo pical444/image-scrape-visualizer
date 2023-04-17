428eb07f30cea7f068ffb785b7821b582166f6ca
pical444 <pcarrascaln@gmail.com>
2023-04-17 15:14:50 +0100

    changes
    import changes from original file

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+ 
+  pixelDensity(1);
+  
+}
+
+
+function draw() {
+ 
+
+ // loadPixels();
+//for (var y = 0; y < height; y++){
+  //for (var x = 0; x < width; x++){
+   // var index = (x + y * width)*4;
+   // pixels[index+0] = x;
+   // pixels[index+1] = 90;
+  //pixels[index+2] = y;
+    //pixels[index+3] = 255;
+ // }
+//}
+
+
+  //updatePixels();
+  
+
+}
